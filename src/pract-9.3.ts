cb8bf6b368f2c09453725ccb339c507c72799725
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-05-06 08:44:14 +0600

    change 9.3

--- src/pract-9.3.ts
+++ src/pract-9.3.ts
@@ -1,16 +1,16 @@
 {
-  const createArrayWithTuple = <A, B>(param1: A, param2: B): [A, B] => {
-    return [param1, param2];
-  };
+    const createArrayWithTuple = <A, B>(param1: A, param2: B): [A, B] => {
+        return [param1, param2];
+    };
 
-  const simpleArrayWithTuple = createArrayWithTuple<string, number>(
-    "tuple",
-    123
-  );
-  const simpleObjectWithTuple = createArrayWithTuple<string, { name: string }>(
-    "objecttuple",
-    {
-      name: "Practice Object with Tuple",
-    }
-  );
+    const simpleArrayWithTuple = createArrayWithTuple<string, number>(
+        "tuple",
+        123,
+    );
+    const simpleObjectWithTuple = createArrayWithTuple<
+        string,
+        { name: string }
+    >("objecttuple", {
+        name: "Practice Object with Tuple",
+    });
 }
